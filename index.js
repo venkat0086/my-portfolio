401274b62530eb694d0e19662ed4d6392d823632
venkatr0086 <venkatesh.r@getshopse.com>
2024-07-18 18:21:17 +0530

    projects and experience updated

--- index.js
+++ index.js
@@ -3,23 +3,22 @@
  ---------------------------------------- */
 
 const handleFirstTab = (e) => {
-  if(e.key === 'Tab') {
-    document.body.classList.add('user-is-tabbing')
+  if (e.key === "Tab") {
+    document.body.classList.add("user-is-tabbing");
 
-    window.removeEventListener('keydown', handleFirstTab)
-    window.addEventListener('mousedown', handleMouseDownOnce)
+    window.removeEventListener("keydown", handleFirstTab);
+    window.addEventListener("mousedown", handleMouseDownOnce);
   }
-
-}
+};
 
 const handleMouseDownOnce = () => {
-  document.body.classList.remove('user-is-tabbing')
+  document.body.classList.remove("user-is-tabbing");
 
-  window.removeEventListener('mousedown', handleMouseDownOnce)
-  window.addEventListener('keydown', handleFirstTab)
-}
+  window.removeEventListener("mousedown", handleMouseDownOnce);
+  window.addEventListener("keydown", handleFirstTab);
+};
 
-window.addEventListener('keydown', handleFirstTab)
+window.addEventListener("keydown", handleFirstTab);
 
 const backToTopButton = document.querySelector(".back-to-top");
 let isBackToTopRendered = false;
@@ -41,3 +40,33 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+let currentIndex = 0;
+
+const carouselInner = document.querySelector(".carousel-inner");
+const carouselItems = document.querySelectorAll(".carousel-item");
+const totalItems = carouselItems.length;
+
+function showNextImage() {
+  currentIndex = (currentIndex + 1) % totalItems;
+  updateCarousel();
+}
+
+function updateCarousel() {
+  const offset = -currentIndex * 100;
+  carouselInner.style.transform = `translateX(${offset}%)`;
+}
+
+// Automatically move to the next image every 3 seconds
+setInterval(showNextImage, 3000);
+
+function downloadApk() {
+  var apkUrl =
+    "https://drive.google.com/uc?export=download&id=1dB5tRsdvmclwSLCyIHuZ9FQXhq-7CP9V";
+  var link = document.createElement("a");
+  link.href = apkUrl;
+  link.download = "ShadowB.jpg";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
